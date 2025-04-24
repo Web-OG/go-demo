8af4b1e9c0a85a303741faab1aea7eb1c1c0cb23
ivan <lostnemo@rambler.ru>
2025-04-24 18:37:17 +0300

    добавляет повтор ввода

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 
 func main() {
 	fmt.Println("____Калькулятор индекса массы тела____")
-	var userHeight, userKg = getUserInput()
-	IMT := calculateIMT(userKg, userHeight)
-	outputResult(IMT)
+	for {
+		var userHeight, userKg = getUserInput()
+		IMT := calculateIMT(userKg, userHeight)
+		outputResult(IMT)
+		isRepeat := isRepeatCheck()
+		if !isRepeat {
+			break
+		}
+	}
 }
 
 func outputResult(imt float64) {
-	res := fmt.Sprintf("ваш индекс массы тела: %.0f", imt)
+	res := fmt.Sprintf("Ваш индекс массы тела: %.0f.", imt)
 	fmt.Print(res)
+	if imt < 16 {
+		fmt.Println(" У вас недостаток веса.")
+	}
 }
 
 func calculateIMT(kg float64, height float64) float64 {
@@ -25,10 +34,20 @@ func calculateIMT(kg float64, height float64) float64 {
 func getUserInput() (float64, float64) {
 	var userHeight float64
 	var userKg float64
-	fmt.Print("Введите ваш рос: ")
+	fmt.Print("Введите ваш рост: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите ваш вес: ")
 	fmt.Scan(&userKg)
 
 	return userHeight, userKg
+}
+
+func isRepeatCheck() bool {
+	var answer string
+	fmt.Println("Повтрорить расчет (y/n)?")
+	fmt.Scan(&answer)
+	if answer == "y" || answer == "Y" {
+		return true
+	}
+	return false
 }
