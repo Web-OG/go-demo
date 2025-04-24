c9677312201204e4d7ab35af700d1ddb2acbf565
ivan <lostnemo@rambler.ru>
2025-04-24 16:30:14 +0300

    выносит вывод принта результата в отдельную функцию

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	const IMTPower = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Println("____Калькулятор индекса массы тела____")
+	fmt.Print("Введите ваш рос: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите ваш вес: ")
+	fmt.Scan(&userKg)
+	var IMT = userKg / math.Pow(userHeight/100, IMTPower)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	res := fmt.Sprintf("ваш индекс массы тела: %.0f", imt)
+	fmt.Print(res)
 }
