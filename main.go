7a1eebd1674684bee217d228d646a4ae2f1550b4
ivan <lostnemo@rambler.ru>
2025-04-25 16:35:49 +0300

    добавляет обработку ошибок

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -8,7 +9,11 @@ import (
 func main() {
 	fmt.Println("____Калькулятор индекса массы тела____")
 	for {
-		var userHeight, userKg = getUserInput()
+		var userHeight, userKg, err = getUserInput()
+		fmt.Printf("Произошла ошибка: %v \n", err)
+		if err != nil {
+			break
+		}
 		IMT := calculateIMT(userKg, userHeight)
 		outputResult(IMT)
 		isRepeat := isRepeatCheck()
@@ -31,21 +36,30 @@ func calculateIMT(kg float64, height float64) float64 {
 	return kg / math.Pow(height/100, IMTPower)
 }
 
-func getUserInput() (float64, float64) {
+func getUserInput() (float64, float64, error) {
 	var userHeight float64
 	var userKg float64
 	fmt.Print("Введите ваш рост: ")
-	fmt.Scan(&userHeight)
+	_, err := fmt.Scan(&userHeight)
+	if err != nil {
+		return 0, 0, errors.New("Не верный ввод роста")
+	}
 	fmt.Print("Введите ваш вес: ")
-	fmt.Scan(&userKg)
+	_, err = fmt.Scan(&userKg)
+	if err != nil {
+		return 0, 0, errors.New("Не верный ввод веса")
+	}
 
-	return userHeight, userKg
+	return userHeight, userKg, nil
 }
 
 func isRepeatCheck() bool {
 	var answer string
 	fmt.Println("Повтрорить расчет (y/n)?")
-	fmt.Scan(&answer)
+	_, err := fmt.Scan(&answer)
+	if err != nil {
+		return false
+	}
 	if answer == "y" || answer == "Y" {
 		return true
 	}
